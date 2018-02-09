03a9340aa3ded2842e87556b9a180701eeae845d
BrandonALittle <brandon.a.little@gmail.com>
2018-02-08 17:19:37 -0800

    complete -- without promises

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -10,12 +10,50 @@ exports.headers = {
   'Content-Type': 'text/html'
 };
 
-exports.serveAssets = function(res, asset, callback) {
-  // Write some code here that helps serve up your static files!
-  // (Static files are things like html (yours or archived from others...),
-  // css, or anything that doesn't change often.)
+exports.sendResponse = function(response, data, statusCode) {
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, exports.headers);
+  response.end(data);
 };
 
+exports.collectData = function(request, callback) {
+  var data = '';
+  request.on('data', function(chunk) {
+    data += chunk;
+  });
+  request.on('end', function () {
+    callback(data);
+  });
+};
 
+exports.serveAssets = function(res, asset, callback) {
+
+  var encoding = {encoding: 'utf8'};
+  fs.readFile( archive.paths.siteAssets + asset, encoding, function(err, data) {
+    if (err) {
+      // file doesn't exist in public!
+      fs.readFile( archive.paths.archivedSites + asset, encoding, function(err, data) {
+        if (err) {
+          // file doesn't exist in archive!
+          callback ? callback() : exports.send404(res);
+        } else {
+          exports.sendResponse(res, data);
+        }
+      });
+    } else {
+      exports.sendResponse(res, data);
+    }
+  });
+};
+
+exports.sendRedirect = function(response, location, status) {
+  status = status || 302;
+  response.writeHead(status, {Location: location});
+  response.end();
+};
+
+exports.send404 = function(response) {
+  exports.sendResponse(response, '404: Page not found', 404);
+};
 
 // As you progress, keep thinking about what helper functions you can put here!
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,13 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+var archive = require('../helpers/archive-helpers');
+archive.readListOfUrls(archive.downloadUrls);
+
+//crontab script scheduler wakes up htmlFetcher
+// wakes up
+// helper functions needed :
+// readUrlList
+// isUrlInArchive
+// downloadUrl
+
